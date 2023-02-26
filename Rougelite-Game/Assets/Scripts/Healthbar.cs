c12c263e838bc54da47a21e4a4cc607339eea1f3
snappylolz <113632664+snappylolz@users.noreply.github.com>
2023-02-25 21:36:21 -0500

    no message

--- Rougelite-Game/Assets/Scripts/Healthbar.cs
+++ Rougelite-Game/Assets/Scripts/Healthbar.cs
@@ -4,20 +4,43 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+
 public class Healthbar : MonoBehaviour
 {
-    [SerializeField] private health playerHealth;
-    [SerializeField] private Image totalHealthbar;
-    [SerializeField] private Image currentHealthbar;
-
-
-    private void Start()
-    {
-        totalHealthbar.fillAmount = playerHealth.currentHealth / 10;
-    }
+    public int health;
+    public int NumOfHearts;
+    
+    
+    public Image[] Burgers;
+    public Sprite FullBurger;
+    public Sprite EmptyBurger;
 
     private void Update()
     {
-        currentHealthbar.fillAmount = playerHealth.currentHealth / 10;
+        if (health > NumOfHearts)
+        {
+            health = NumOfHearts;
+        }
+
+        for (int i = 0; i < Burgers.Length; i++)
+        {
+            if (i < health)
+            {
+                Burgers[i].sprite = FullBurger;
+            }
+            else
+            {
+                Burgers[i].sprite = EmptyBurger;
+            }
+
+            if (i < NumOfHearts)
+            {
+                Burgers[i].enabled = true;
+            }
+            else
+            {
+                Burgers[i].enabled = false;
+            }
+        }
     }
 }

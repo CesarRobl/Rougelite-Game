654b9a6a82bec7fbff46ebc16e99253de4fb49b2
Aidan Byrne <ajbyrne99@gmail.com>
2023-04-02 17:49:38 -0400

    Update for April 2nd

--- Rougelite-Game/Assets/Scripts/PlayerScripts/PlayerSprite.cs
+++ Rougelite-Game/Assets/Scripts/PlayerScripts/PlayerSprite.cs
@@ -25,15 +25,20 @@ public class PlayerSprite : MonoBehaviour
     void ChangeSpriteDir()
     {
 
-        if (GMController.gm.holder.eulerAngles.z >= 180)
+        if (GMController.gm.holder.eulerAngles.z >225 && GMController.gm.holder.eulerAngles.z<=315)
         {
             sr.sprite = chardir[0];
             //spatula.transform.position = new Vector3(spatula.transform.position.x, spatula.transform.position.y, -.5f);
         }
-        else if (GMController.gm.holder.eulerAngles.z < 180)
+        else if (GMController.gm.holder.eulerAngles.z <= 45 || GMController.gm.holder.eulerAngles.z > 315)
+        {
+            Debug.Log("FaceRight");
+        }
+        else if(GMController.gm.holder.eulerAngles.z>135&&GMController.gm.holder.eulerAngles.z<=225)
+            Debug.Log("FaceLeft");
+        else if (GMController.gm.holder.eulerAngles.z >45&&GMController.gm.holder.eulerAngles.z<=135)
         {
             sr.sprite = chardir[1];
-            //spatula.transform.position = new Vector3(spatula.transform.position.x, spatula.transform.position.y, 0);
         }
     }
 }
